9ec0f51f79beb8ae8379d38fb1e8c865b8ae3277
1LeandroLV <leandro.verdugo02@gmail.com>
2026-03-13 16:57:43 +0100

    Add project files.

---
+++ easy-jwt.identity-exercise/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace easy_jwt.identity_exercise
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
