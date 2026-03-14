7abe9e207064d8d57392bd48a47ae8eb8e9d21b2
1LeandroLV <leandro.verdugo02@gmail.com>
2026-03-14 17:51:39 +0100

    Appusercs och enrollment.cs fillerna är klara

--- easy-jwt.identity-exercise/Models/Appuser.cs
+++ easy-jwt.identity-exercise/Models/Appuser.cs
@@ -1,6 +1,12 @@
-﻿namespace easy_jwt.identity_exercise.Models
-{
-    public class Appuser
+﻿using Microsoft.AspNetCore.Identity; //packet som jag kommer behöva för denna klassen
+//denna klass anväds för att representera alla anvädare i systemet, bygger på
+//identitys färdiga anvädare 
+
+
+namespace easy_jwt.identity_exercise.Models
+{  //anvädare i systemet
+    public class Appuser : IdentityUser
     {
+        
     }
 }
--- easy-jwt.identity-exercise/Models/Enrollment.cs
+++ easy-jwt.identity-exercise/Models/Enrollment.cs
@@ -1,6 +1,11 @@
 ﻿namespace easy_jwt.identity_exercise.Models
+    //denna klass anväds för att begränsa status till gilitiga värden:
 {
-    public class Enrollment
+    public enum Enrollment
     {
+        // student har ansökt, vänta på godkännande
+        peding,
+        //student ska få tillgång till kursen enligt reglerna 
+        Approved
     }
 }
