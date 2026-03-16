d99ea767a2cd890054f028fc4c7c28e22a8ffe76
1LeandroLV <leandro.verdugo02@gmail.com>
2026-03-16 13:39:07 +0100

    skapat Appdbcontext

--- easy-jwt.identity-exercise/Data/AppDbContext.cs
+++ easy-jwt.identity-exercise/Data/AppDbContext.cs
@@ -1,6 +1,42 @@
-﻿namespace easy_jwt.identity_exercise.Data
+﻿using easy_jwt.identity_exercise.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace easy_jwt.identity_exercise.Data
 {
-    public class AppDbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }    
+          public DbSet<Course> Courses { get; set; }
+          public DbSet<Enrollments> Enrollments { get; set; }
+
+
+        // används för att konfiguera databasen; hur realtioner fungera,
+        //vilka foreign keys som finns, unika regler, extra konfiguration
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //kör fört identitys egen dabas konfiguration
+            //glömmer man den kan identity tabller sluta fungera 
+            base.OnModelCreating(builder);
+
+            builder.Entity<Course>() //konfiguera modellen Course
+            .HasOne(C => c.Teacher) //en course har en teacher
+            .withMany(); //en teacher kan ha många courses 
+
+            builder.Entity<Enrollments>() //konfiguera modelln Enrollment
+            .HasOne(e => e.Course)//en course har en course
+            .WithMany(c => c.Enrollments); //Course kan ha många Enrollments
+
+            builder.Entity<Enrollments>()
+            .HasOne(e => e.Student)
+            .WithMany();
+
+            builder.Entity<Enrollments>()
+            .HasIndex(e => new { e.CourseId, e.StudentId })
+            .IsUnique();
+        }
     }
 }
